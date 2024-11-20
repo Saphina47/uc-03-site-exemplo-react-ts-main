dd4b98e3da7651c26f91f722a05f8b1dc0eda68a
Jocelyn Laurent <saphina47@gmail.com>
2024-11-19 21:49:00 -0300

    exercicios aula

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,64 @@
-console.log("ola mundo")
+// Arrays ,Listae Tuplas
+
+const tuplas:[string , string, number , boolean]=[
+"Jocelyn Laurent"
+"Jocelynlaurent.@gmail.com",
+"30"
+true
+
+]
+const tuplas:[string , string, number , boolean][]-[
+ [ "jocelyn laurent", "joelynlaurent@gmail.com",34 true]
+["saphina@gmail.com", "Edmir", 34 , true],
+]
+
+// nome ,email, idade, ativo
+const nome = tupla[1][0],
+
+//Hash table - tabela de chave/valor
+
+type pessoa = {
+  nome :string
+  email:string
+  idade :number
+  ativo :boolean
+  celular :string
+}
+
+const pessoa: Pessoa[] ={
+  nome :"jocelyn Laurent"
+  email:"jocelynlaurent@gmaol.com"
+  idade: 34
+  ativo:true
+}
+{email:"saphina@gmail.com",
+nome:"Edmir"
+idade:34
+ativo:true},
+
+for   (const pessoa of pessoas) {
+  conlole.log(pessoa["email"]);
+}
+
+const tuplas:[string , string, number , boolean][]-[
+[nome, email,data ,]
+["notebook"];475635 , 25 true ,doi de abril de dois mil vente,
+["celular"]
+["televisão"]
+["tablete"]
+["bateria"]
+["cabo"]
+["desktops"]
+["macbook"]
+["caixa de som"]
+[""]
+
+
+type Produto = {
+  id:1
+  nome:notebook,
+  quantidde:5,0,
+  ativo :true,
+  datacriação:11/11/2024,
+  dataatualização:11/11/2024
+}
